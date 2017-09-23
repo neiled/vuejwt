a383cc59a0cb3662f3a6fe228f97540d44b52b7a
Neil Edwards <neil@plasticwater.com>
2017-09-23 12:59:29 -0400

    start writing the login component

--- website/src/components/Login.vue
+++ website/src/components/Login.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
+    <label>Email</label>
     <input type="email" name="user_email" id="user_email" value="" />
+    <label>Password</label>
     <input type="password" name="user_password" id="user_password" value="" />
-
   </div>
 </template>
 
