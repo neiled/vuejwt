5e274c77bdc850f7bc01b2e6b0848313ee81fa76
Neil Edwards <neil@plasticwater.com>
2017-09-23 13:17:39 -0400

    added bootstrap support

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="container">
+      <img src="./assets/logo.png">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
--- website/src/components/Login.vue
+++ website/src/components/Login.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <label>Email</label>
-    <input type="email" name="user_email" id="user_email" value="" />
-    <label>Password</label>
-    <input type="password" name="user_password" id="user_password" value="" />
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <div class="form-check">
+        <label class="form-check-label">
+          <input type="checkbox" class="form-check-input">
+          Check me out
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
