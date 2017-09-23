b2d3fe8e2c3d584cbbf3acf10d116d21da8928a3
Neil Edwards <neil@plasticwater.com>
2017-09-23 12:50:14 -0400

    move babel and add Login component

---
+++ website/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="email" name="user_email" id="user_email" value="" />
+    <input type="password" name="user_password" id="user_password" value="" />
+
+  </div>
+</template>
+
+<script>
+  export default{
+
+  }
+</script>
+<style scoped>
+
+</style>
