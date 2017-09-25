c7f0d425fabd772f1a57c301d3c271ce1b610355
Neil Edwards <neil@plasticwater.com>
2017-09-25 15:27:53 -0400

    Add auth and axios

--- website/src/main.js
+++ website/src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import auth from './lib/auth'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.headers.common['Authorization'] = auth.getAuthHeader()
 
 Vue.use(BootstrapVue)
 
