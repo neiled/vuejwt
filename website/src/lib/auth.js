0e86d592c13088c7d226e8d48c55181d6693f730
Neil Edwards <neil@plasticwater.com>
2017-09-25 21:38:37 -0400

    continued with auth

--- website/src/lib/auth.js
+++ website/src/lib/auth.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 export default {
   login () {
     console.log('auth.login - NEEDS CODE')
-    axios.defaults.headers.common['Authorization'] = auth.getAuthHeader()
+    axios.defaults.headers.common['Authorization'] = this.getAuthHeader()
   },
   logout () {
     console.log('auth.logout')
     localStorage.removeItem('id_token')
     this.authenticated = false
-    axios.defaults.headers.common['Authorization'] = auth.getAuthHeader()
+    axios.defaults.headers.common['Authorization'] = this.getAuthHeader()
   },
   checkAuth () {
     console.log('auth.checkAuth')
@@ -23,7 +23,7 @@ export default {
       return 'Bearer ' + token
     } else {
       console.log('auth.getAuthHeader - no token to add to header')
-      return '' 
+      return ''
     }
   }
 }
