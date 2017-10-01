eae8bc164eae086b82885ccc669e986671a6e910
Neil Edwards <neil@plasticwater.com>
2017-10-01 19:52:26 -0400

    added e2e test

--- website/test/end2end/specs/test.js
+++ website/test/end2end/specs/test.js
@@ -1,8 +1,12 @@
 var assert = require('assert')
-describe('webdriver.io page', function() {
+describe('upon initial visit', function() {
+    browser.url('http://10.29.0.15:8080')
     it('should have the right title - the fancy generator way', function () {
-        browser.url('http://10.29.0.15:8080')
         var title = browser.getTitle()
         assert.equal(title, 'Vue JWT Example')
+    })
+    it('should show the user as logged out', function () {
+        var title = $('#app > div > div > h3').getText()
+        assert.equal(title, 'You are currently logged out')
     })
 })
